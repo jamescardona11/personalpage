f55c41fbed552faf89ec1ca0c9e726b7b6d6a02e
jamescardona11 <jjcardonao@gmail.com>
2022-05-01 12:22:00 -0500

    start to create home

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,24 +1,9 @@
-import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import Image from '@/components/Image'
-
-import BlogPostCard from '@/components/BlogPostCard'
-
-const data = {
-  profileImage: '/static/images/avatar.png',
-  name: 'James Cardona',
-  role: 'Software Engineer | ',
-  aboutme:
-    'Tech Explorer | Apasionado por la tecnología, videojuegos y un entusiasta del mundo Cripto y NFTs.',
-  toppics1: '#fluter, #python, #nodejs, #web3, #solidity, #clojure y #next.js.',
-  toppics2: 'Creando aplicaciones, emprendiendo y aprendiendo.',
-}
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -41,38 +26,6 @@ const LayoutWrapper = ({ children }) => {
             <MobileNav />
           </div>
         </header>
-        <div className="flex flex-col-reverse items-start sm:flex-row">
-          <div className="flex flex-col pr-8">
-            <h1 className="mb-1 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
-              {data.name}
-            </h1>
-            <h2 className="mb-4 text-primary-500 dark:text-primary-500">
-              {data.role} <span className="font-semibold">57Blocks</span>
-            </h2>
-            <p className="text-gray-800 dark:text-gray-200">{data.aboutme}</p>
-            <p className="text-sm text-gray-700 dark:text-gray-400">{data.toppics2}</p>
-            <p className="text-sm text-gray-700 dark:text-gray-400">{data.toppics1}</p>
-          </div>
-          <div className="relative mb-8 mr-auto w-[80px] sm:mb-0 sm:w-[176px]">
-            <Image
-              alt="James Cardona"
-              height={176}
-              width={176}
-              src={data.profileImage}
-              className="rounded-full grayscale filter"
-            />
-          </div>
-        </div>
-        <button className="mt-3 mb-16 inline-flex items-center rounded bg-primary-600 py-3 px-6 font-bold text-white hover:bg-primary-800">
-          <svg
-            className="mr-2 h-4 w-4 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-          </svg>
-          <span>Download CV</span>
-        </button>
         <main className="mb-auto">{children}</main>
         <Footer />
       </div>
--- components/me.js
+++ components/me.js
@@ -1,70 +1,53 @@
 import Image from '@/components/Image'
-import Link from '@/components/Link'
+// import Link from '@/components/Link'
+import SectionContainer from './SectionContainer'
 
 const data = {
   profileImage: '/static/images/avatar.png',
   name: 'James Cardona',
-  role: 'Software Engineer',
-  description:
-    "I'm a frontend developer based in Bekasi, Indonesia. I'm passionate about building web applications with Reactjs",
+  role: 'Software Engineer | ',
+  aboutme:
+    'Tech Explorer | Apasionado por la tecnología, videojuegos y un entusiasta del mundo Cripto y NFTs.',
+  toppics1: '#fluter, #python, #nodejs, #web3, #solidity, #clojure y #next.js.',
+  toppics2: 'Creando aplicaciones, emprendiendo y aprendiendo.',
 }
 
 const Me = () => {
   return (
-    <section className="min-w-screen grid items-center justify-between py-8 lg:grid-cols-2 lg:grid-rows-1">
-      <div className="mb-10 flex h-full w-full flex-col items-start justify-center pr-8 xl:mb-0">
-        <div className="flex flex-col items-center justify-start md:flex-row">
-          {/* <Image
-            src="/static/images/avatar.png"
-            alt="An image about James Cardona"
-            className="h-32 w-32 rounded-full border-2 border-gray-200 shadow-md"
-            width={72}
-            height={72}
-            layout="fixed"
-            quality={60}
-            priority
-            loading="eager"
-          /> */}
-
-          <figure className="relative mb-4 h-32 w-32 rounded-full sm:h-44 sm:w-44 md:mb-0 md:mr-12 lg:mr-32">
-            <Image
-              src={data.profileImage}
-              quality={80}
-              width={200}
-              height={200}
-              alt="Andrian Fadhilla"
-              layout="intrinsic"
-              loading="eager"
-              className="h-full w-full rounded-full"
-            />
-          </figure>
-          <h2 className="font-display text-3xl font-extrabold leading-tight sm:text-4xl sm:leading-none md:text-5xl lg:text-5xl xl:text-5xl">
+    <SectionContainer>
+      <div className="flex flex-col-reverse items-start sm:flex-row">
+        <div className="flex flex-col pr-8">
+          <h1 className="mb-1 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
             {data.name}
+          </h1>
+          <h2 className="mb-4 text-primary-500 dark:text-primary-500">
+            {data.role} <span className="font-semibold">57Blocks</span>
           </h2>
+          <p className="text-gray-800 dark:text-gray-200">{data.aboutme}</p>
+          <p className="text-sm text-gray-700 dark:text-gray-400">{data.toppics2}</p>
+          <p className="text-sm text-gray-700 dark:text-gray-400">{data.toppics1}</p>
         </div>
-        <h2 className="text-primary my-2 font-semibold text-purple-400">{data.role}</h2>
-        <p className="mt-2 text-lg ">
-          Tech Explorer | Apasionado por la tecnología, videojuegos y un entusiasta del mundo Cripto
-          y NFTs.
-        </p>
-        <p className="mt-3 text-sm text-gray-400 dark:text-gray-400">
-          Fluter, python, nodejs, web3, Solidity, Entusiasta de Clojure y Next.js.
-        </p>
-        <p className="text-sm text-gray-400 dark:text-gray-400">
-          Creando algunas aplicaciones, emprendiendo y aprendiendo.
-        </p>
-        <button className="mt-3 inline-flex items-center rounded bg-purple-600 py-3 px-6 font-bold text-white hover:bg-purple-800">
-          <svg
-            className="mr-2 h-4 w-4 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-          </svg>
-          <span>Download CV</span>
-        </button>
+        <div className="relative mb-8 mr-auto w-[80px] sm:mb-0 sm:w-[176px]">
+          <Image
+            alt="James Cardona"
+            height={176}
+            width={176}
+            src={data.profileImage}
+            className="rounded-full grayscale filter"
+          />
+        </div>
       </div>
-    </section>
+      <button className="mt-3 mb-16 inline-flex items-center rounded bg-primary-600 py-3 px-6 font-bold text-white hover:bg-primary-800">
+        <svg
+          className="mr-2 h-4 w-4 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+        </svg>
+        <span>Download CV</span>
+      </button>
+    </SectionContainer>
   )
 }
 
