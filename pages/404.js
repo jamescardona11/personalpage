100963e1e24db896ca76e47a67fb69e293d7ce07
jamescardona11 <jjcardonao@gmail.com>
2022-06-25 12:37:47 -0500

    404 page done

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 export default function FourZeroFour() {
   return (
     <>
-      <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
+      <PageSEO title={`Pagina no encontrada - ${siteMetadata.title}`} />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
@@ -14,14 +14,14 @@ export default function FourZeroFour() {
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
+            Hey!, no pudimos encontrar la pagina.
           </p>
           <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+            Pero no te preocupes, puedes encontrar muchas más cosas en nuestro homepage.
           </p>
           <Link href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
+            <button className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-primary-700 focus:outline-none dark:hover:bg-primary-500">
+              Vuelve al homepage
             </button>
           </Link>
         </div>
